831fd68ea8d65772f69ca6f7fb8756da69fd8e40
BMartinPla <129972087+BMartinPla@users.noreply.github.com>
2023-08-25 11:24:55 -0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,11 @@
+var nombre = "Juan";
+var apellido = "Fernandez";
+console.log(nombre +" "+ apellido);
+
+var numero1 = 5;
+var numero2 = 10;
+var suma = numero1 + numero2
+console.log(suma);
+
+var suma2 = suma + 15;
+console.log(suma2);
